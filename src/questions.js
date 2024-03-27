dd0dfbdbd652c5c2b0ebc84f5f8bc397860b3582
unknown <pedroelton>
2024-03-27 11:14:00 +0900

    Game files

---
+++ src/questions.js
@@ -0,0 +1,147 @@
+export const questions = [
+  {
+    questionText: "What word rhymes with CAT?",
+    answerOptions: [
+      { answerText: "Dog", isCorrect: false },
+      { answerText: "Mouse", isCorrect: false },
+      { answerText: "Hat", isCorrect: true },
+      { answerText: "Milk", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What word rhymes with Dog?",
+    answerOptions: [
+      { answerText: "Bone", isCorrect: false },
+      { answerText: "Frog", isCorrect: true },
+      { answerText: "Tail", isCorrect: false },
+      { answerText: "Food", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What word rhymes with Play?",
+    answerOptions: [
+      { answerText: "May", isCorrect: true },
+      { answerText: "July", isCorrect: false },
+      { answerText: "School", isCorrect: false },
+      { answerText: "Toy", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What word rhymes with Green?",
+    answerOptions: [
+      { answerText: "Grapes", isCorrect: false },
+      { answerText: "Princess", isCorrect: false },
+      { answerText: "King", isCorrect: false },
+      { answerText: "Queen", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "What word rhymes with Blue?",
+    answerOptions: [
+      { answerText: "Sky", isCorrect: false },
+      { answerText: "Pants", isCorrect: false },
+      { answerText: "Shoe", isCorrect: true },
+      { answerText: "Socks", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What word rhymes with Cake?",
+    answerOptions: [
+      { answerText: "Shake", isCorrect: true },
+      { answerText: "Juice", isCorrect: false },
+      { answerText: "Chocolate", isCorrect: false },
+      { answerText: "Eat", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What word rhymes with House?",
+    answerOptions: [
+      { answerText: "Chair", isCorrect: false },
+      { answerText: "Street", isCorrect: false },
+      { answerText: "Bedroom", isCorrect: false },
+      { answerText: "Mouse", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "Who built a house of straw in the Three Little Pigs?",
+    answerOptions: [
+      { answerText: "The wolf", isCorrect: false },
+      { answerText: "The first pig", isCorrect: true },
+      { answerText: "The farmer", isCorrect: false },
+      { answerText: "The grandma", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What did the tortoise use to beat the hare in the race?",
+    answerOptions: [
+      { answerText: "His strength", isCorrect: false },
+      { answerText: "His cunning", isCorrect: false },
+      { answerText: "His slowness", isCorrect: false },
+      { answerText: "His perseverance", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "What did Cinderella leave behind at the ball?",
+    answerOptions: [
+      { answerText: "Her shoe", isCorrect: true },
+      { answerText: "Her dress", isCorrect: false },
+      { answerText: "Her pumpkin", isCorrect: false },
+      { answerText: "Her mask", isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'Who cried "Wolf!" too many times?',
+    answerOptions: [
+      { answerText: "The sheep", isCorrect: false },
+      { answerText: "The hunter", isCorrect: false },
+      { answerText: "The little boy", isCorrect: true },
+      { answerText: "The grandma", isCorrect: false },
+    ],
+  },
+  
+  {
+    questionText: "What is the house made of in the Three Little Pigs Story?",
+    answerOptions: [
+      { answerText: "Straw, Wood and Bricks", isCorrect: true },
+      { answerText: "Candy, Chocolate and Cookies", isCorrect: false },
+      { answerText: "Wood, Sand and Concrete", isCorrect: false },
+      { answerText: "Construction Material", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What lesson does a fable usually teach?",
+    answerOptions: [
+      { answerText: "A funny story", isCorrect: false },
+      { answerText: "A scary story", isCorrect: false },
+      { answerText: "A lesson to learn", isCorrect: true },
+      { answerText: "A long poem", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What are colorful pictures with a story called?",
+    answerOptions: [
+      { answerText: "Books", isCorrect: false },
+      { answerText: "Comics", isCorrect: true },
+      { answerText: "Fables", isCorrect: false },
+      { answerText: "Songs", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What is the main character in a story called?",
+    answerOptions: [
+      { answerText: "The monster", isCorrect: false },
+      { answerText: "The hero", isCorrect: true },
+      { answerText: "The ending", isCorrect: false },
+      { answerText: "The title", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What happens at the end of a story?",
+    answerOptions: [
+      { answerText: "The beginning", isCorrect: false },
+      { answerText: "The middle", isCorrect: false },
+      { answerText: "The ending", isCorrect: true },
+      { answerText: "The pictures", isCorrect: false },
+    ],
+  },
+];
